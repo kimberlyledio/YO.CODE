94110219cce6352773335dcd7b21567bb35f6f53
Kimberly Lédio <92836426+kimberlyledio@users.noreply.github.com>
2021-12-13 13:29:27 -0300

    yocodecleber

---
+++ yocodecleber/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <!-- CSS  -->
+		<meta charset="UTF-8">
+		<title>YO.CODE</title>
+			<link href="css/estilos.css" type="text/css" rel="stylesheet">
+			<!-- CSS only -->
+        	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+	</head>
+    <body>
+      <div class="col-lg-">
+          <div class="row">
+			<div class="col-sm- div"></div>
+			<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+				<div class="container-fluid">
+					<a href="#" class="navbar-brand">
+						<img src="img/digitallogo.jpeg" height="28ss" alt="YO.CODE"> YO.CODE
+					</a>
+					<button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarCollapse">
+						<div class="navbar-nav">
+							<a href="index.php" class="nav-item nav-link active">Home</a>
+							<a href="quemsomos.php" class="nav-item nav-link">Quem somos</a>
+							<a href="cadastro.php" class="nav-item nav-link">Cadastre-se</a>
+						</div>
+					</div>
+				</div> 
+			 </nav>
+			 <div class="col-sm- div2"></div>
+             <div class="col-sm- div4">
+                <img src="img/bannerrrrr.png" width="100%" height="100%">
+            </div> 
+            
+            <div class="col-sm- div5">
+                <br/>
+                <div class="titulos">
+                    NOSSOS SERVIÇOS
+                </div>
+
+                <div class="textos">
+					Somos uma empresa de desenvolvimento Web, e estamos prontas para tirar a sua idéia do papel <br/>
+					com nosso design para páginas web e nossos serviços de Desenvolvimento Back-end! <br/>
+					Também estamos dispostas a dar suporte técnico para o seu negócio!<br/>
+					Bem-Vindo(a) a nossa empresa YO.CODE Tec!
+                </div>
+            </div>
+            <div class="col-sm- div6">
+                <section class="content-sites">
+                </section>
+            </div>
+            <br/>
+			<div class="col-sm- div10"></div>
+            <div class="col-sm- div7">
+                <div id="ladoa"> 
+		<center>
+		<div class="titulos">
+			MISSÃO
+		</div> 
+		<br/>
+		<img src="img/missao.png" width="30%" height="30%">
+		<br/>
+		<div class="textos">
+			<br/>
+			Fornecer um site de qualidade <br/>
+			que leve e expresse a ideia do cliente.
+		</div>
+	</center>
+	</div> 
+	   <div id="ladob">
+		<center> <div class="titulos">
+			VISÃO
+		</div> 
+		<img src="img/visao.png" width="30%" height="30%">
+		<br/>
+		<div class="textos">
+			<br/>
+			Crescer no ramo da tecnologia, <br/>
+			para sempre atendermos os sonhos <br/>
+			e projetos dos nossos clientes <br/>
+			com excelência e originalidade.
+		</div>
+	
+	    </center>
+	    </div>
+	   <div id="ladoc">
+		<div class="titulos">
+			<center> VALORES</center>
+		</div>
+	<center><img src="img/valor.png" width="30%" height="30%"> </center>
+		<br/>
+		<div class="textos">
+			<center> O profissionalismo com um atendimento
+		     de qualidade, respeitando sempre a singularidade de cada clientes,<br/>
+			para o melhor desenvolvimento dos nossos sites.</center>
+		</div>
+            </div>
+
+             </div>
+             <div class="col-sm- div8"></div>
+             <div class="col-sm- div9">
+				<div class="footer-content">
+					<div class="titulos2">Sobre </div>
+						<p1 style="font-size: 20px; font-family: arial; color: #fff; justify-content: left;">
+				 Desenvolvemos sites para a web. <br>
+				 Escolha o modelo e nós fazemos para você!<br>
+				 Tornando o sonho dos clientes em realiade.<br>
+						</p1>
+						<div class="titulos2">
+					Contato
+						</div>
+					<p1 style="font-size: 20px; font-family: arial; color: #fff;">
+				 Email: yo.codetec@gmail.com.br <br>
+				Instagram: @yo.code_tec <br>
+				Contato: (11)99518-1580 <br/>
+			   (11)94769-4719 <br/>
+			   (11)98020-8491
+			</p1>
+		<br>
+		
+			<ul class="logos">
+				<li><a href="#" target="_blank"><img src="img/insta.png" alt="Instagram" width="50px" height="50px"></a></li>
+				<li><a href="#" target="_blank"><img src="img/facebook.png" alt="facebook"width="50px" height="50px"></a></li>
+		   
+			</ul>
+			<p style="font-size: 20px; font-family: Arial; color: #fff;">
+			&copy; Bianca Toledo, Kimberly Lédio e Luana Alves </p>
+                 </div> 
+
+
+          </div>
+      </div>
+
+
+
+
+
+
+
+    </body>
+
+
+
+
+</html>
