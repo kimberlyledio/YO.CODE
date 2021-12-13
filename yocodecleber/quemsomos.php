168861d00065c4aab2fff118f9a16929dabb5384
Kimberly Lédio <92836426+kimberlyledio@users.noreply.github.com>
2021-12-13 13:01:39 -0300

    Add files via upload

---
+++ yocodecleber/quemsomos.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <!-- CSS  -->
+		<meta charset="UTF-8">
+		<title>YO.CODE</title>
+			<link href="css/estilos.css" type="text/css" rel="stylesheet">
+			<!-- CSS only -->
+        	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+	</head>
+    <body>
+      <div class="col-lg-">
+          <div class="row">
+			<div class="col-sm- div"></div>
+			<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+				<div class="container-fluid">
+					<a href="#" class="navbar-brand">
+						<img src="img/digitallogo.jpeg" height="28ss" alt="YO.CODE"> YO.CODE
+					</a>
+					<button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarCollapse">
+						<div class="navbar-nav">
+							<a href="index.php" class="nav-item nav-link active">Home</a>
+							<a href="quemsomos.php" class="nav-item nav-link">Quem somos</a>
+							<a href="cadastro.php" class="nav-item nav-link">Cadastre-se</a>
+
+						</div>
+					</div>
+				</div>
+			 </nav>
+			 <div class="col-sm- div2"></div>
+             <div class="col-sm- div4">
+                <img src="img/bannerrrrr.png" width="100%" height="100%">
+            </div> 
+            <div class="col-sm- div10">
+            </div>
+            <div class="col-sm- div5">
+                <div class="titulos">
+                    Quem somos
+                    <img src="img/pessoa.png" width="40px" height="40px">
+                </div>
+                    
+                <div class="textos">
+                    Nós somos estudantes do Instituto Federal de Guarulhos e atualmente estamos cursando <br/>
+					o 3°ano do Ensino Médio Técnico em informática para Internet! Pelo interesse em comum<br/>
+					em informática e através de um trabalho em grupo, do qual desenvolvemos um jogo para <br/>
+					disléxicos que foi campeão de 2 medalhas de prata pela feira da Exatecca e da Feceg.<br/>
+				    Assim, percemos que nossas ideias juntamente com a nossa amizade formavam uma<br/>
+					ótima que poderá trazer uma empresa de qualidade para o ambiente tecnológico!<br/>
+					Foi então, que criamos a YO.CODE Tec!
+                    </div>
+            </div>
+
+			<div class="col-sm- div7">
+				<div id="fundo" style="background: url(Design\ sem\ nome\ \(1\).png); width: 100%; height: 800px">
+					<div id="autoraa"> 
+						<div class="imgredonda">
+							<center>
+							<img src="img/Bianca.jpeg" width="100%" height="100%">
+						</div>
+							<br/>
+								<center>
+								<div class="titulos">
+									<br/>
+									Bianca Toledo
+								</div>
+								</center>
+					
+							</center>
+						</div> 
+
+					<div id="autorab">
+						<div class="imgredonda">
+							<center> 
+							<img src="img/Kimberly.jpeg" width="100%" height="100%">
+						</div>
+							<br/>
+								<center>
+								<div class="titulos">
+									<br/>
+									Kimberly Lédio
+								</div>
+								</center>
+					
+							</center>
+						</div>
+
+					<div id="autorac">
+						<div class="imgredonda">
+							<center>
+							<img src="img/Luana.jpeg" width="100%" height="100%">
+						</div>
+							<br/>
+								<center>
+								<div class="titulos">
+									<br/>
+									Luana Alves
+								</div>
+								</center>
+						</center>
+					</div>
+				
+				</div>
+			</div>
+
+
+             </div>
+             <div class="col-sm- div8"></div>
+             <div class="col-sm- pag2">
+				<div class="footer-content">
+					<div class="titulos2">Sobre </div>
+						<p1 style="font-size: 20px; font-family: arial; color: #fff; justify-content: left;">
+				 Desenvolvemos sites para a web. <br>
+				 Escolha o modelo e nós fazemos para você!<br>
+				 Tornando o sonho dos clientes em realiade.<br>
+						</p1>
+						<div class="titulos2">
+					Contato
+						</div>
+					<p1 style="font-size: 20px; font-family: arial; color: #fff;">
+				 Email: yo.codetec@gmail.com.br <br>
+				Instagram: @yo.code_tec <br>
+				Contato: (11)99518-1580 <br/>
+			   (11)94769-4719 <br/>
+			   (11)98020-8491
+			</p1>
+		<br>
+		
+			<ul class="logos">
+				<li><a href="#" target="_blank"><img src="img/insta.png" alt="Instagram" width="50px" height="50px"></a></li>
+				<li><a href="#" target="_blank"><img src="img/facebook.png" alt="facebook"width="50px" height="50px"></a></li>
+		   
+			</ul>
+			<p style="font-size: 20px; font-family: Arial; color: #fff;">
+			&copy; Bianca Toledo, Kimberly Lédio e Luana Alves </p>
+                 </div> 
+
+
+          </div>
+      </div>
+
+
+
+
+
+
+
+    </body>
+
+
+
+
+</html>
